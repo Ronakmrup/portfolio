289650f027bfdb60157e3e6d8a0b77b7b0e96b71
Ronakmrup <Ronakmr.up@gmail.com>
2026-05-27 22:34:01 +0530

    Updated config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ronakmrup.github.io',
+  url: 'https://ronakmrup.github.io/portfolio/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/portfolio/',
